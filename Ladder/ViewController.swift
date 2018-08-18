48ed91698b97700580bb7d074dda9be2b411d077
hnkj <1622015523@qq.com>
2018-08-18 18:22:45 +0800

    fix

--- Ladder/ViewController.swift
+++ Ladder/ViewController.swift
@@ -6,18 +6,19 @@
 //  Copyright © 2018 Aofei Sheng. All rights reserved.
 //
 
-import Alamofire
 import Eureka
+import KeychainAccess
 import NetworkExtension
-import SafariServices
-import Security
+import Reachability
 
 class ViewController: FormViewController {
+	let mainKeychain = Keychain(service: Bundle.main.bundleIdentifier!)
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 
 		navigationItem.title = NSLocalizedString("Ladder", comment: "")
-		navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "Icons/Info"), style: .plain, target: self, action: #selector(openPost))
+		navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "Icons/Info"), style: .plain, target: self, action: #selector(openInfo))
 		navigationController?.navigationBar.barStyle = .black
 		navigationController?.navigationBar.tintColor = .white
 		navigationController?.navigationBar.barTintColor = UIColor(red: 80 / 255, green: 140 / 255, blue: 240 / 255, alpha: 1)
@@ -32,33 +33,16 @@ class ViewController: FormViewController {
 			<<< SwitchRow { row in
 				row.tag = "General - Hide VPN Icon"
 				row.title = NSLocalizedString("Hide VPN Icon", comment: "")
-				if let data = ReadFromKeychain(key: "general_hide_vpn_icon") {
-					row.value = String(data: data, encoding: .utf8) == "true"
-				}
+				row.value = mainKeychain["general_hide_vpn_icon"] == "true"
 			}
 			<<< URLRow { row in
 				row.tag = "General - PAC URL"
 				row.title = "PAC URL"
 				row.placeholder = NSLocalizedString("Enter PAC URL here", comment: "")
-				if let data = ReadFromKeychain(key: "general_pac_url") {
-					row.value = URL(string: String(data: data, encoding: .utf8) ?? "https://git.io/gfwpac")
-				}
+				row.value = URL(string: mainKeychain["general_pac_url"] ?? "https://aofei.org/pac?proxies=SOCKS5+127.0.0.1%3A1081%3B+SOCKS+127.0.0.1%3A1081%3B+DIRECT%3B")
 
 				row.add(rule: RuleRequired(msg: NSLocalizedString("Please enter a PAC URL.", comment: "")))
 				row.add(rule: RuleURL(allowsEmpty: false, requiresProtocol: true, msg: NSLocalizedString("Please enter a valid PAC URL.", comment: "")))
-			}
-			<<< IntRow { row in
-				row.tag = "General - PAC Max Age"
-				row.title = NSLocalizedString("PAC Max Age", comment: "")
-				row.placeholder = NSLocalizedString("Enter PAC max age here", comment: "")
-				if let data = ReadFromKeychain(key: "general_pac_max_age") {
-					row.value = Int(String(data: data, encoding: .utf8) ?? "3600")
-				}
-				row.formatter = NumberFormatter()
-
-				row.add(rule: RuleRequired(msg: NSLocalizedString("Please enter a PAC max age.", comment: "")))
-				row.add(rule: RuleGreaterOrEqualThan(min: 0, msg: NSLocalizedString("PAC max age must greater than or equal to 0.", comment: "")))
-				row.add(rule: RuleSmallerOrEqualThan(max: 86400, msg: NSLocalizedString("PAC max age must smaller than or equal to 86400.", comment: "")))
 			}
 
 			+++ Section(header: NSLocalizedString("Shadowsocks", comment: ""), footer: "") { section in
@@ -70,9 +54,7 @@ class ViewController: FormViewController {
 				row.tag = "Shadowsocks - Server Address"
 				row.title = NSLocalizedString("Server Address", comment: "")
 				row.placeholder = NSLocalizedString("Enter server address here", comment: "")
-				if let data = ReadFromKeychain(key: "shadowsocks_server_address") {
-					row.value = String(data: data, encoding: .utf8)
-				}
+				row.value = mainKeychain["shadowsocks_server_address"]
 				row.cell.textField.keyboardType = .asciiCapable
 				row.cell.textField.autocapitalizationType = .none
 
@@ -82,8 +64,8 @@ class ViewController: FormViewController {
 				row.tag = "Shadowsocks - Server Port"
 				row.title = NSLocalizedString("Server Port", comment: "")
 				row.placeholder = NSLocalizedString("Enter server port here", comment: "")
-				if let data = ReadFromKeychain(key: "shadowsocks_server_port"), let string = String(data: data, encoding: .utf8) {
-					row.value = Int(string)
+				if let shadowsocksServerPort = mainKeychain["shadowsocks_server_port"] {
+					row.value = Int(shadowsocksServerPort)
 				}
 				row.formatter = NumberFormatter()
 
@@ -95,9 +77,7 @@ class ViewController: FormViewController {
 				row.tag = "Shadowsocks - Local Address"
 				row.title = NSLocalizedString("Local Address", comment: "")
 				row.placeholder = NSLocalizedString("Enter local address here", comment: "")
-				if let data = ReadFromKeychain(key: "shadowsocks_local_address") {
-					row.value = String(data: data, encoding: .utf8) ?? "127.0.0.1"
-				}
+				row.value = mainKeychain["shadowsocks_local_address"] ?? "127.0.0.1"
 				row.cell.textField.keyboardType = .asciiCapable
 				row.cell.textField.autocapitalizationType = .none
 
@@ -107,9 +87,7 @@ class ViewController: FormViewController {
 				row.tag = "Shadowsocks - Local Port"
 				row.title = NSLocalizedString("Local Port", comment: "")
 				row.placeholder = NSLocalizedString("Enter local port here", comment: "")
-				if let data = ReadFromKeychain(key: "shadowsocks_local_port") {
-					row.value = Int(String(data: data, encoding: .utf8) ?? "1081")
-				}
+				row.value = Int(mainKeychain["shadowsocks_local_port"] ?? "1081")
 				row.formatter = NumberFormatter()
 
 				row.add(rule: RuleRequired(msg: NSLocalizedString("Please enter a Shadowsocks local port.", comment: "")))
@@ -120,9 +98,7 @@ class ViewController: FormViewController {
 				row.tag = "Shadowsocks - Password"
 				row.title = NSLocalizedString("Password", comment: "")
 				row.placeholder = NSLocalizedString("Enter password here", comment: "")
-				if let data = ReadFromKeychain(key: "shadowsocks_password") {
-					row.value = String(data: data, encoding: .utf8)
-				}
+				row.value = mainKeychain["shadowsocks_password"]
 
 				row.add(rule: RuleRequired(msg: NSLocalizedString("Please enter a Shadowsocks password.", comment: "")))
 			}
@@ -131,9 +107,7 @@ class ViewController: FormViewController {
 				row.title = NSLocalizedString("Method", comment: "")
 				row.selectorTitle = NSLocalizedString("Shadowsocks Method", comment: "")
 				row.options = ["AES-128-CFB", "AES-192-CFB", "AES-256-CFB", "ChaCha20", "Salsa20", "RC4-MD5"]
-				if let data = ReadFromKeychain(key: "shadowsocks_method") {
-					row.value = String(data: data, encoding: .utf8) ?? "AES-256-CFB"
-				}
+				row.value = mainKeychain["shadowsocks_method"] ?? "AES-256-CFB"
 				row.cell.detailTextLabel?.textColor = .black
 			}
 
@@ -153,7 +127,7 @@ class ViewController: FormViewController {
 				)
 				self.present(configuringAlertController, animated: true)
 
-				if let reachable = Alamofire.NetworkReachabilityManager(host: "8.8.8.8")?.isReachable, !reachable {
+				if Reachability()!.connection == .none {
 					let alertController = UIAlertController(
 						title: NSLocalizedString("Configuration Failed", comment: ""),
 						message: NSLocalizedString("Please check your network settings and allow Ladder to access your wireless data in the system's \"Settings - Cellular\" option (remember to check the \"WLAN & Cellular Data\").", comment: ""),
@@ -186,93 +160,74 @@ class ViewController: FormViewController {
 					return
 				}
 
-				let generalHideVPNIcon = (self.form.rowBy(tag: "General - Hide VPN Icon") as! SwitchRow).value!
-				let generalPACURL = (self.form.rowBy(tag: "General - PAC URL") as! URLRow).value!
-				let generalPACMaxAge = (self.form.rowBy(tag: "General - PAC Max Age") as! IntRow).value!
-				let shadowsocksServerAddress = (self.form.rowBy(tag: "Shadowsocks - Server Address") as! TextRow).value!
-				let shadowsocksServerPort = (self.form.rowBy(tag: "Shadowsocks - Server Port") as! IntRow).value!
-				let shadowsocksLocalAddress = (self.form.rowBy(tag: "Shadowsocks - Local Address") as! TextRow).value!
-				let shadowsocksLocalPort = (self.form.rowBy(tag: "Shadowsocks - Local Port") as! IntRow).value!
-				let shadowsocksPassword = (self.form.rowBy(tag: "Shadowsocks - Password") as! PasswordRow).value!
-				let shadowsocksMethod = (self.form.rowBy(tag: "Shadowsocks - Method") as! ActionSheetRow<String>).value!
-
-				Alamofire.request(generalPACURL).responseString { response in
-					if response.response?.statusCode != 200 || response.value == nil {
-						let alertController = UIAlertController(
-							title: NSLocalizedString("Configuration Failed", comment: ""),
-							message: NSLocalizedString("Unable to download data from the PAC URL.", comment: ""),
-							preferredStyle: .alert
-						)
-						alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default))
-						configuringAlertController.dismiss(animated: true) {
-							self.present(alertController, animated: true)
-						}
-						return
-					}
-
-					NETunnelProviderManager.loadAllFromPreferences { providerManagers, _ in
-						var providerManager = NETunnelProviderManager()
-						if let providerManagers = providerManagers, providerManagers.count > 0 {
-							providerManager = providerManagers[0]
-							if providerManagers.count > 1 {
-								for providerManager in providerManagers[1...] {
-									providerManager.removeFromPreferences()
-								}
+				NETunnelProviderManager.loadAllFromPreferences { providerManagers, _ in
+					var providerManager = NETunnelProviderManager()
+					if let providerManagers = providerManagers, providerManagers.count > 0 {
+						providerManager = providerManagers[0]
+						if providerManagers.count > 1 {
+							for index in 1 ..< providerManagers.count {
+								providerManagers[index].removeFromPreferences()
 							}
 						}
+					}
 
-						let providerConfiguration = NETunnelProviderProtocol()
-						providerConfiguration.serverAddress = shadowsocksServerAddress
-						providerConfiguration.providerConfiguration = [
-							"general_hide_vpn_icon": generalHideVPNIcon,
-							"general_pac_url": generalPACURL.absoluteString,
-							"general_pac_content": response.value!,
-							"general_pac_max_age": TimeInterval(generalPACMaxAge),
-							"shadowsocks_server_address": shadowsocksServerAddress,
-							"shadowsocks_server_port": UInt16(shadowsocksServerPort),
-							"shadowsocks_local_address": shadowsocksLocalAddress,
-							"shadowsocks_local_port": UInt16(shadowsocksLocalPort),
-							"shadowsocks_password": shadowsocksPassword,
-							"shadowsocks_method": shadowsocksMethod,
-						]
+					let generalHideVPNIcon = (self.form.rowBy(tag: "General - Hide VPN Icon") as! SwitchRow).value!
+					let generalPACURL = (self.form.rowBy(tag: "General - PAC URL") as! URLRow).value!
+					let shadowsocksServerAddress = (self.form.rowBy(tag: "Shadowsocks - Server Address") as! TextRow).value!
+					let shadowsocksServerPort = UInt16((self.form.rowBy(tag: "Shadowsocks - Server Port") as! IntRow).value!)
+					let shadowsocksLocalAddress = (self.form.rowBy(tag: "Shadowsocks - Local Address") as! TextRow).value!
+					let shadowsocksLocalPort = UInt16((self.form.rowBy(tag: "Shadowsocks - Local Port") as! IntRow).value!)
+					let shadowsocksPassword = (self.form.rowBy(tag: "Shadowsocks - Password") as! PasswordRow).value!
+					let shadowsocksMethod = (self.form.rowBy(tag: "Shadowsocks - Method") as! ActionSheetRow<String>).value!
 
-						providerManager.localizedDescription = NSLocalizedString("Ladder", comment: "")
-						providerManager.protocolConfiguration = providerConfiguration
-						providerManager.isEnabled = true
-						providerManager.saveToPreferences { error in
-							if error == nil {
-								self.WriteToKeychain(key: "general_hide_vpn_icon", data: (generalHideVPNIcon ? "true" : "false").data(using: .utf8))
-								self.WriteToKeychain(key: "general_pac_url", data: generalPACURL.absoluteString.data(using: .utf8))
-								self.WriteToKeychain(key: "general_pac_max_age", data: String(generalPACMaxAge).data(using: .utf8))
-								self.WriteToKeychain(key: "shadowsocks_server_address", data: shadowsocksServerAddress.data(using: .utf8))
-								self.WriteToKeychain(key: "shadowsocks_server_port", data: String(shadowsocksServerPort).data(using: .utf8))
-								self.WriteToKeychain(key: "shadowsocks_local_address", data: shadowsocksLocalAddress.data(using: .utf8))
-								self.WriteToKeychain(key: "shadowsocks_local_port", data: String(shadowsocksLocalPort).data(using: .utf8))
-								self.WriteToKeychain(key: "shadowsocks_password", data: shadowsocksPassword.data(using: .utf8))
-								self.WriteToKeychain(key: "shadowsocks_method", data: shadowsocksMethod.data(using: .utf8))
-								providerManager.loadFromPreferences { error in
-									if error == nil {
-										providerManager.connection.stopVPNTunnel()
-										DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-											try? providerManager.connection.startVPNTunnel()
-										}
+					let providerConfiguration = NETunnelProviderProtocol()
+					providerConfiguration.serverAddress = "Ladder"
+					providerConfiguration.providerConfiguration = [
+						"general_hide_vpn_icon": generalHideVPNIcon,
+						"general_pac_url": generalPACURL.absoluteString,
+						"shadowsocks_server_address": shadowsocksServerAddress,
+						"shadowsocks_server_port": shadowsocksServerPort,
+						"shadowsocks_local_address": shadowsocksLocalAddress,
+						"shadowsocks_local_port": shadowsocksLocalPort,
+						"shadowsocks_password": shadowsocksPassword,
+						"shadowsocks_method": shadowsocksMethod,
+					]
+
+					providerManager.localizedDescription = NSLocalizedString("Ladder", comment: "")
+					providerManager.protocolConfiguration = providerConfiguration
+					providerManager.isEnabled = true
+					providerManager.saveToPreferences { error in
+						if error == nil {
+							self.mainKeychain["general_hide_vpn_icon"] = generalHideVPNIcon ? "true" : "false"
+							self.mainKeychain["general_pac_url"] = generalPACURL.absoluteString
+							self.mainKeychain["shadowsocks_server_address"] = shadowsocksServerAddress
+							self.mainKeychain["shadowsocks_server_port"] = String(stringInterpolationSegment: shadowsocksServerPort)
+							self.mainKeychain["shadowsocks_local_address"] = shadowsocksLocalAddress
+							self.mainKeychain["shadowsocks_local_port"] = String(stringInterpolationSegment: shadowsocksLocalPort)
+							self.mainKeychain["shadowsocks_password"] = shadowsocksPassword
+							self.mainKeychain["shadowsocks_method"] = shadowsocksMethod
+							providerManager.loadFromPreferences { error in
+								if error == nil {
+									providerManager.connection.stopVPNTunnel()
+									DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+										try? providerManager.connection.startVPNTunnel()
 									}
 								}
 							}
-							configuringAlertController.dismiss(animated: true) {
-								let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
-								if error != nil {
-									alertController.title = NSLocalizedString("Configuration Failed", comment: "")
-									alertController.message = NSLocalizedString("Please try again.", comment: "")
-									alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default))
-								} else {
-									alertController.title = NSLocalizedString("Configured!", comment: "")
-								}
-								self.present(alertController, animated: true) {
-									if error == nil {
-										DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-											alertController.dismiss(animated: true)
-										}
+						}
+						configuringAlertController.dismiss(animated: true) {
+							let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+							if error != nil {
+								alertController.title = NSLocalizedString("Configuration Failed", comment: "")
+								alertController.message = NSLocalizedString("Please try again.", comment: "")
+								alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default))
+							} else {
+								alertController.title = NSLocalizedString("Configured!", comment: "")
+							}
+							self.present(alertController, animated: true) {
+								if error == nil {
+									DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) {
+										alertController.dismiss(animated: true)
 									}
 								}
 							}
@@ -282,29 +237,7 @@ class ViewController: FormViewController {
 			}
 	}
 
-	@objc func openPost() {
-		present(SFSafariViewController(url: URL(string: "https://aofei.org/posts/2018-04-05-immersive-wallless-experience")!), animated: true)
-	}
-
-	@discardableResult
-	func ReadFromKeychain(key: String) -> Data? {
-		var query = [CFString: Any]()
-		query[kSecClass] = kSecClassGenericPassword
-		query[kSecAttrAccount] = key
-		query[kSecReturnData] = kCFBooleanTrue
-		query[kSecMatchLimit] = kSecMatchLimitOne
-		var data: CFTypeRef?
-		SecItemCopyMatching(query as CFDictionary, &data)
-		return data as? Data
-	}
-
-	@discardableResult
-	func WriteToKeychain(key: String, data: Data?) -> Bool {
-		var query = [CFString: Any]()
-		query[kSecClass] = kSecClassGenericPassword
-		query[kSecAttrAccount] = key
-		query[kSecValueData] = data
-		SecItemDelete(query as CFDictionary)
-		return data != nil ? SecItemAdd(query as CFDictionary, nil) == noErr : true
+	@objc func openInfo() {
+		//UIApplication.shared.openURL(URL(string: "https://aofei.org/posts/2018-04-05-immersive-wallless-experience")!)
 	}
 }
